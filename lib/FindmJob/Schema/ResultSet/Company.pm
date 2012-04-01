32b8f0fbbeb547c4ef1864256e1ba34bf7df17d7
Fayland Lam <fayland@gmail.com>
2012-04-01 11:30:04 +0800

    add company

---
+++ lib/FindmJob/Schema/ResultSet/Company.pm
@@ -0,0 +1,32 @@
+package FindmJob::Schema::ResultSet::Company;
+
+use Moose;
+use namespace::autoclean;
+extends 'FindmJob::Schema::ResultSet';
+
+sub get {
+    my ($self, $row) = @_;
+
+    if ($row->{website}) {
+        my $r = $self->get_by_website( $row->{website} );
+        return $r if $r;
+    }
+    my $r = $self->get_by_name( $row->{name} );
+    return $r if $r;
+    return $self->create($row);
+}
+
+sub get_by_website {
+    my ($self, $website) = @_;
+
+    return $self->search( { website => $website } )->first;
+}
+
+sub get_by_name {
+    my ($self, $name) = @_;
+
+    return $self->search( { name => $name } )->first;
+}
+
+
+1;
--- lib/FindmJob/Schema/ResultSet/Job.pm
+++ lib/FindmJob/Schema/ResultSet/Job.pm
@@ -13,7 +13,9 @@ sub is_inserted_by_url {
 sub create_job {
     my ($self, $row) = @_;
 
-    my $company = delete $row->{company};
+    my $schema = $self->result_source->schema;
+    my $company = $schema->resultset('Company')->get(delete $row->{company});
+    $row->{company_id} = $company->{id};
     $row->{inserted_at} = time();
     $self->create($row);
 }
