59f6377ddfdeaf37971881a8d65ee3177507dc84
Fayland Lam <fayland@gmail.com>
2012-04-01 12:44:23 +0800

    init www

--- www/bin/app.pl
+++ www/bin/app.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/env perl
+
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib";
+use lib "$Bin/../../lib";
 use Dancer;
 use FindmJob::WWW;
+
 dance;
--- www/lib/FindmJob/WWW.pm
+++ www/lib/FindmJob/WWW.pm
@@ -9,8 +9,20 @@ use FindmJob::Basic;
 setting 'views'  => path( FindmJob::Basic->root, 'templates' );
 setting 'public' => path( FindmJob::Basic->root, 'static' );
 
+hook before_template_render => sub {
+    my $tokens = shift;
+
+    # merge vars into token b/c I like it more
+    my $vars = delete $tokens->{vars} || {};
+    foreach (keys %$vars) {
+        $tokens->{$_} = $vars->{$_};
+    }
+
+    $tokens->{config} = FindmJob::Basic->config;
+};
+
 get '/' => sub {
-    template 'index';
+    template 'index.tt2';
 };
 
 true;
