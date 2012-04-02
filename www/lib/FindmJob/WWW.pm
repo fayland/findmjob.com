469629d440a28f28fc8c9bd47292171594d06b49
Fayland Lam <fayland@gmail.com>
2012-04-02 22:50:11 +0800

    get rough rss/atom support in

--- www/lib/FindmJob/WWW.pm
+++ www/lib/FindmJob/WWW.pm
@@ -7,6 +7,7 @@ our $VERSION = '0.1';
 use FindmJob::Basic;
 use Encode;
 use JSON::XS ();
+use Dancer::Plugin::Feed;
 
 # different template dir than default one
 setting 'views'  => path( FindmJob::Basic->root, 'templates' );
@@ -32,6 +33,14 @@ get qr'.*?/p\.(\d+).*?' => sub {
     forward $uri;
 };
 
+get qr'.*?/feed\.(rss|atom).*?' => sub {
+    my $uri = request->uri;
+    $uri =~ s'/feed\.(rss|atom)'';
+    var feed_format => $1;
+    $uri =~ s/\/$//;
+    forward $uri;
+};
+
 get '/' => sub {
     my $schema = FindmJob::Basic->schema;
     my $p = vars->{page} || 1; $p = 1 unless $p =~ /^\d+$/;
@@ -40,8 +49,15 @@ get '/' => sub {
         rows => 12,
         page => $p
     });
+    my @jobs = $job_rs->all;
     var pager => $job_rs->pager;
-    var jobs  => [ $job_rs->all ];
+    var jobs  => \@jobs;
+
+    if (vars->{feed_format}) {
+        var title => "Recent Jobs - FindmJob.com";
+        map { $_->{tbl} = 'job' } @jobs;
+        return _render_feed(@jobs);
+    }
 
     template 'index.tt2';
 };
@@ -119,7 +135,51 @@ get '/tag/:tagid' => sub {
     var pager => $rs->pager;
     var objects => \@obj;
 
+    if (vars->{feed_format}) {
+        var title => "$tag Jobs - FindmJob.com";
+        return _render_feed(@obj);
+    }
+
     template 'tag.tt2';
 };
+
+sub _render_feed {
+    my (@obj) = @_;
+
+    my $config = FindmJob::Basic->config;
+
+    my @entries;
+    foreach my $obj (@obj) {
+        my ($title, $content, $link, $author);
+        # refer templates/object.tt2
+        if ($obj->{tbl} eq 'job') {
+            $link = $config->{sites}->{main} . "/job/" . $obj->id;
+            $title = $obj->title;
+            $author = $obj->company->name;
+            $content = $obj->description;
+            if ($obj->source_url !~ /jobs.github.com/) {
+                $content =~ s/\n/\<br\/>/sg;
+            }
+        } elsif ($obj->{tbl} eq 'company') {
+            $link = $config->{sites}->{main} . "/company/" . $obj->id;
+            $title = $obj->name;
+        }
+
+        push @entries, {
+            id => $obj->id,
+            link => $link,
+            title => $title,
+            $author ? (author => $author) : (),
+            $content ? (content => $content) : (),
+        };
+    }
+
+    my $feed = create_feed(
+        format  => vars->{feed_format},
+        title   => vars->{title},
+        entries => \@entries,
+    );
+    return $feed;
+}
 
 true;
