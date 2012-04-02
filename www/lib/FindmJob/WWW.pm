3d1e6a69a53df5b9e514d4e0c6514b3d1f006f82
Fayland Lam <fayland@gmail.com>
2012-04-02 19:10:23 +0800

    line order matters

--- www/lib/FindmJob/WWW.pm
+++ www/lib/FindmJob/WWW.pm
@@ -27,8 +27,8 @@ hook before_template_render => sub {
 get qr'.*?/p\.(\d+).*?' => sub {
     my $uri = request->uri;
     $uri =~ s'/p.([^\/]+)'';
-    $uri =~ s/\/$//;
     var page => $1;
+    $uri =~ s/\/$//;
     forward $uri;
 };
 
