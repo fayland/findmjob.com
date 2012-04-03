c1314bd631cc68c32b1b98753c0f6758ebddf158
Fayland Lam <fayland@gmail.com>
2012-04-03 14:41:18 +0800

    seo for sitemap too

--- cron/tools/sitemap.pl
+++ cron/tools/sitemap.pl
@@ -11,6 +11,8 @@ my $root = FindmJob::Basic->root;
 my $file = "$root/static/sitemap.xml.gz";
 
 my $map = WWW::Sitemap::XML->new();
+my $config = FindmJob::Basic->config;
+my $schema = FindmJob::Basic->schema;
 my $dbh = FindmJob::Basic->dbh;
 
 my @d = localtime();
@@ -25,7 +27,9 @@ $map->add(
 my $sth = $dbh->prepare("SELECT id, tbl FROM object ORDER BY time DESC LIMIT 500");
 $sth->execute();
 while (my ($id, $tbl) = $sth->fetchrow_array) {
-    my $url = 'http://findmjob.com/' . $tbl . '/' . $id;
+    my $obj = $schema->resultset(ucfirst $tbl)->find($id);
+    my $url = $config->{sites}->{main} . '/' . $tbl . '/' . $id;
+    $url = $config->{sites}->{main} . $obj->url if $obj->can('url');
     if ($tbl eq 'job') {
         $map->add(
             loc => $url,
