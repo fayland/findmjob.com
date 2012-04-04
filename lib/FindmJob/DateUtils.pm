4c77984cb721a16bd55cee6ef664af70461f52b0
Fayland Lam <fayland@gmail.com>
2012-04-04 11:24:28 +0800

    using localtime

--- lib/FindmJob/DateUtils.pm
+++ lib/FindmJob/DateUtils.pm
@@ -10,7 +10,7 @@ use Date::Manip::Date;
 
 sub human_to_db_date {
     my ($in) = @_;
-    $in = scalar(gmtime($in)) if $in =~ /^\d+$/;
+    $in = scalar(localtime($in)) if $in =~ /^\d+$/;
     my $date = Date::Manip::Date->new;
     $date->parse($in);
     return $date->printf('%Y-%m-%d');
@@ -18,7 +18,7 @@ sub human_to_db_date {
 
 sub human_to_db_datetime {
     my ($in) = @_;
-    $in = scalar(gmtime($in)) if $in =~ /^\d+$/;
+    $in = scalar(localtime($in)) if $in =~ /^\d+$/;
     my $date = Date::Manip::Date->new;
     $date->parse($in);
     return $date->printf('%Y-%m-%d %H:%M:%S');
