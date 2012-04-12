3326658d35249d9b39d11b4cb9279d499abb9604
Fayland Lam <fayland@gmail.com>
2012-04-12 17:04:27 +0800

    make api search working

--- api/lib/FindmJob/Resource.pm
+++ api/lib/FindmJob/Resource.pm
@@ -4,11 +4,9 @@ use strict;
 use warnings;
 use parent 'Web::Machine::Resource';
 
-use JSON::XS ();
+use JSON::XS 'encode_json';
 use FindmJob::Basic;
 use FindmJob::Search;
-
-my $JSON = JSON::XS->new->allow_nonref->pretty;
 
 # read-only API for now
 sub content_types_provided { [{ 'application/json' => 'to_json'   }] }
@@ -40,18 +38,7 @@ sub _dispatcher {
         my $id = shift @args;
         my $job = $schema->resultset('Job')->find($id);
         return 0 unless $job;
-
-        # only known cols are exported
-        foreach my $col ('id', 'source_url', 'title', 'location', 'description', 'type', 'contact', 'inserted_at', 'expired_at') {
-            $data->{$col} = $job->get_column($col);
-        }
-
-        # related company and tags
-        my @tags = @{ $job->tags };
-        @tags = map { $_->{text} } @tags;
-        $data->{tag} = \@tags;
-        my $company = $job->company;
-        $data->{company} = { id => $company->id, name => $company->name, website => $company->website };
+        $data = _export_job_as_hashref($job);
     } elsif ($action eq 'search') {
         my $q = $req->param('q');
         my $p = $req->param('page'); $p = 1 unless $p and $p =~ /^\d+$/;
@@ -61,11 +48,51 @@ sub _dispatcher {
             rows => 12,
             page => $p,
         } );
+        my @data;
+        if ($ret->{total}) {
+            my @jobids = map { $_->{id} } @{$ret->{matches}};
+            my $schema = FindmJob::Basic->schema;
+            my @jobs   = $schema->resultset('Job')->search( {
+                id => { 'IN', \@jobids }
+            } )->all;
+            my %jobs = map { $_->id => $_ } @jobs;
+            @jobs = map { $jobs{$_} } @jobids;
+
+            foreach my $job (@jobs) {
+                push @data, _export_job_as_hashref($job);
+            }
+            $data->{jobs}  = \@data;
+            $data->{total} = $ret->{total};
+            $data->{page}  = $p;
+        } else {
+            $data->{jobs} = [];
+            $data->{total} = 0;
+        }
     }
     $self->{_data} = $data;
     return 1;
 }
 
-sub to_json { $JSON->encode( (shift)->{_data} ) }
+sub _export_job_as_hashref {
+    my $job = shift;
+
+    my $data;
+
+    # only known cols are exported
+    foreach my $col ('id', 'source_url', 'title', 'location', 'description', 'type', 'contact', 'inserted_at', 'expired_at') {
+        $data->{$col} = $job->get_column($col);
+    }
+
+    # related company and tags
+    my @tags = @{ $job->tags };
+    @tags = map { $_->{text} } @tags;
+    $data->{tag} = \@tags;
+    my $company = $job->company;
+    $data->{company} = { id => $company->id, name => $company->name, website => $company->website };
+
+    return $data;
+}
+
+sub to_json { encode_json( (shift)->{_data} ) }
 
 1;
