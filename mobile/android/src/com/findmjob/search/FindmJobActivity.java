9e7196261c03fb9d116f0a473721ac7cd133fde6
Fayland Lam <fayland@gmail.com>
2012-04-13 20:09:06 +0800

    test init android app

---
+++ mobile/android/src/com/findmjob/search/FindmJobActivity.java
@@ -0,0 +1,13 @@
+package com.findmjob.search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindmJobActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
