bc143790c683c61dab8d4ec4e334d55d3c066189
Fayland Lam <fayland@gmail.com>
2012-04-14 16:26:26 +0800

    use jsonp to work

--- mobile/android/assets/www/mobile.coffee
+++ mobile/android/assets/www/mobile.coffee
@@ -1,36 +1,30 @@
 $(document).bind "mobileinit", () ->
+    console.log 'mobileinit'
     ## Make your jQuery Mobile framework configuration changes here!
     $.mobile.allowCrossDomainPages = true
 
-$(document).ready ->
-    $('#debug').html 'start'
-
+$(document).bind 'pageinit', () ->
     ## for search
-    $('#search_btn').bind 'click', (e) ->
+    $('.search_btn').bind 'click', (e) ->
+        $('#search_loading').show()
+        $('#search_result').html("")
+
         e.preventDefault()
-        $('#debug').html 'onclick'
         q = $.trim $('input[name="q"]').val()
         loc = $.trim $('input[name="loc"]').val()
-        $('#debug').html q
-        $('#debug').html loc
+        console.log q
+        console.log loc
         return false unless q.length or loc.length
-        console.log 'xxxx'
-        $('#debug').html 'submitting'
-        $.ajax {
-            url: 'http://api.findmjob.com/search',
-            type: 'GET',
-            dataType: 'json',
-            timeout: 5000,
-            data: { q: q, loc: loc },
-            success: (data) ->
-                $('#search_result').empty();
-                for job in data.jobs
-                    $('#search_result').append """<p><a class='job' id='#{job.id}' data-role="button">#{job.title}</a></p>"""
-            error: (xhr, ajaxOptions, thrownError) ->
-                console.log xhr.statusText
-                $('#debug').html 'failed'
-        }
-    @
-
+        console.log 'submitting'
+        $.getJSON 'http://api.findmjob.com/search?q=' + q + '&loc=' + loc + '&callback=?', (data) ->
+            show_search_results(data)
 
 @
+
+show_search_results = (data) ->
+    console.log data
+    $('#search_loading').hide()
+    html = ''
+    for job in data.jobs
+        html += """<li><a class='job' id='#{job.id}' data-role="button">#{job.title}</a></li>"""
+    $('#search_result').html(html).listview("destroy").listview()
--- mobile/android/assets/www/mobile.js
+++ mobile/android/assets/www/mobile.js
@@ -1,51 +1,42 @@
 (function() {
+  var show_search_results;
 
   $(document).bind("mobileinit", function() {
+    console.log('mobileinit');
     return $.mobile.allowCrossDomainPages = true;
   });
 
-  $(document).ready(function() {
-    $('#debug').html('start');
-    $('#search_btn').bind('click', function(e) {
+  $(document).bind('pageinit', function() {
+    return $('.search_btn').bind('click', function(e) {
       var loc, q;
+      $('#search_loading').show();
+      $('#search_result').html("");
       e.preventDefault();
-      $('#debug').html('onclick');
       q = $.trim($('input[name="q"]').val());
       loc = $.trim($('input[name="loc"]').val());
-      $('#debug').html(q);
-      $('#debug').html(loc);
+      console.log(q);
+      console.log(loc);
       if (!(q.length || loc.length)) return false;
-      console.log('xxxx');
-      $('#debug').html('submitting');
-      return $.ajax({
-        url: 'http://api.findmjob.com/search',
-        type: 'GET',
-        dataType: 'json',
-        timeout: 5000,
-        data: {
-          q: q,
-          loc: loc
-        },
-        success: function(data) {
-          var job, _i, _len, _ref, _results;
-          $('#search_result').empty();
-          _ref = data.jobs;
-          _results = [];
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            job = _ref[_i];
-            _results.push($('#search_result').append("<p><a class='job' id='" + job.id + "' data-role=\"button\">" + job.title + "</a></p>"));
-          }
-          return _results;
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          console.log(xhr.statusText);
-          return $('#debug').html('failed');
-        }
+      console.log('submitting');
+      return $.getJSON('http://api.findmjob.com/search?q=' + q + '&loc=' + loc + '&callback=?', function(data) {
+        return show_search_results(data);
       });
     });
-    return this;
   });
 
   this;
+
+  show_search_results = function(data) {
+    var html, job, _i, _len, _ref;
+    console.log(data);
+    $('#search_loading').hide();
+    html = '';
+    _ref = data.jobs;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      job = _ref[_i];
+      html += "<li><a class='job' id='" + job.id + "' data-role=\"button\">" + job.title + "</a></li>";
+    }
+    return $('#search_result').html(html).listview("destroy").listview();
+  };
 
 }).call(this);
