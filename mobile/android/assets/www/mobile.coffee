7c6df991c9127b1c672f568e2262453ef78e930e
Fayland Lam <fayland@gmail.com>
2012-04-14 17:54:16 +0800

    page refresh is an issue

--- mobile/android/assets/www/mobile.coffee
+++ mobile/android/assets/www/mobile.coffee
@@ -1,30 +1,48 @@
 $(document).bind "mobileinit", () ->
-    console.log 'mobileinit'
     ## Make your jQuery Mobile framework configuration changes here!
     $.mobile.allowCrossDomainPages = true
 
 $(document).bind 'pageinit', () ->
     ## for search
     $('.search_btn').bind 'click', (e) ->
-        $('#search_loading').show()
-        $('#search_result').html("")
+        $.mobile.showPageLoadingMsg()
+        $('#search_result').html ""
+        $('#job_detail').html ""
 
         e.preventDefault()
         q = $.trim $('input[name="q"]').val()
         loc = $.trim $('input[name="loc"]').val()
-        console.log q
-        console.log loc
         return false unless q.length or loc.length
         console.log 'submitting'
         $.getJSON 'http://api.findmjob.com/search?q=' + q + '&loc=' + loc + '&callback=?', (data) ->
             show_search_results(data)
 
-@
-
 show_search_results = (data) ->
-    console.log data
-    $('#search_loading').hide()
+    $(document).jqmData('jobs', data.jobs)
+    $.mobile.hidePageLoadingMsg()
     html = ''
     for job in data.jobs
-        html += """<li><a class='job' id='#{job.id}' data-role="button">#{job.title}</a></li>"""
-    $('#search_result').html(html).listview("destroy").listview()
+        html += """
+                <li><a onclick="javascript: show_job('#{job.id}')" id='#{job.id}'>#{job.title}</a></li>
+                """
+    $('#search_result').html(html).listview("destroy").listview().show()
+
+show_job = (id) ->
+    $('#search_result').hide()
+    jobs = $(document).jqmData('jobs')
+    for job in jobs
+        if job.id == id
+            description = job.description.replace("\n", "<br />")
+            html = """
+                    <h3>#{job.title}</h3>
+                    <p>#{job.description}#</p>
+                    <input type='button' onclick="javascript:back_search()" data-role="button" value="Back to Search" />
+                   """
+            $('#job_detail').html(html)
+            ## $('#job_detail').trigger( 'updatelayout' );
+            $('#job_detail input[data-role=button]').button()
+            break
+
+back_search = () ->
+    $('#search_result').show()
+    $('#job_detail').html ""
--- mobile/android/assets/www/mobile.js
+++ mobile/android/assets/www/mobile.js
@@ -1,42 +1,60 @@
-(function() {
-  var show_search_results;
+var back_search, show_job, show_search_results;
 
-  $(document).bind("mobileinit", function() {
-    console.log('mobileinit');
-    return $.mobile.allowCrossDomainPages = true;
-  });
+$(document).bind("mobileinit", function() {
+  return $.mobile.allowCrossDomainPages = true;
+});
 
-  $(document).bind('pageinit', function() {
-    return $('.search_btn').bind('click', function(e) {
-      var loc, q;
-      $('#search_loading').show();
-      $('#search_result').html("");
-      e.preventDefault();
-      q = $.trim($('input[name="q"]').val());
-      loc = $.trim($('input[name="loc"]').val());
-      console.log(q);
-      console.log(loc);
-      if (!(q.length || loc.length)) return false;
-      console.log('submitting');
-      return $.getJSON('http://api.findmjob.com/search?q=' + q + '&loc=' + loc + '&callback=?', function(data) {
-        return show_search_results(data);
-      });
+$(document).bind('pageinit', function() {
+  return $('.search_btn').bind('click', function(e) {
+    var loc, q;
+    $.mobile.showPageLoadingMsg();
+    $('#search_result').html("");
+    $('#job_detail').html("");
+    e.preventDefault();
+    q = $.trim($('input[name="q"]').val());
+    loc = $.trim($('input[name="loc"]').val());
+    if (!(q.length || loc.length)) return false;
+    console.log('submitting');
+    return $.getJSON('http://api.findmjob.com/search?q=' + q + '&loc=' + loc + '&callback=?', function(data) {
+      return show_search_results(data);
     });
   });
+});
 
-  this;
+show_search_results = function(data) {
+  var html, job, _i, _len, _ref;
+  $(document).jqmData('jobs', data.jobs);
+  $.mobile.hidePageLoadingMsg();
+  html = '';
+  _ref = data.jobs;
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    job = _ref[_i];
+    html += "<li><a onclick=\"javascript: show_job('" + job.id + "')\" id='" + job.id + "'>" + job.title + "</a></li>";
+  }
+  return $('#search_result').html(html).listview("destroy").listview().show();
+};
 
-  show_search_results = function(data) {
-    var html, job, _i, _len, _ref;
-    console.log(data);
-    $('#search_loading').hide();
-    html = '';
-    _ref = data.jobs;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      job = _ref[_i];
-      html += "<li><a class='job' id='" + job.id + "' data-role=\"button\">" + job.title + "</a></li>";
+show_job = function(id) {
+  var description, html, job, jobs, _i, _len, _results;
+  $('#search_result').hide();
+  jobs = $(document).jqmData('jobs');
+  _results = [];
+  for (_i = 0, _len = jobs.length; _i < _len; _i++) {
+    job = jobs[_i];
+    if (job.id === id) {
+      description = job.description.replace("\n", "<br />");
+      html = "<h3>" + job.title + "</h3>\n<p>" + job.description + "#</p>\n<input type='button' onclick=\"javascript:back_search()\" data-role=\"button\" value=\"Back to Search\" />";
+      $('#job_detail').html(html);
+      $('#job_detail').trigger('updatelayout');
+      break;
+    } else {
+      _results.push(void 0);
     }
-    return $('#search_result').html(html).listview("destroy").listview();
-  };
+  }
+  return _results;
+};
 
-}).call(this);
+back_search = function() {
+  $('#search_result').show();
+  return $('#job_detail').html("");
+};
