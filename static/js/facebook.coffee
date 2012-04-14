0bbd253307ace611ef41afb717f5b06a6192ab26
Fayland Lam <fayland@gmail.com>
2012-04-14 13:51:10 +0800

    not going to wait for ready

--- static/js/facebook.coffee
+++ static/js/facebook.coffee
@@ -1,15 +1,14 @@
-$(document).ready ->
-    ## facebook app
-    window.fbAsyncInit = () ->
-        FB.init {
-            appId : '281749461905114',
-            status : true,
-            cookie : true,
-            xfbml : true
-        }
-        FB.Canvas.setAutoGrow();
+## facebook app
+window.fbAsyncInit = () ->
+    FB.init {
+        appId : '281749461905114',
+        status : true,
+        cookie : true,
+        xfbml : true
+    }
+    FB.Canvas.setAutoGrow();
 
-    e = document.createElement('script');
-    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-    e.async = true;
-    document.getElementById('fb-root').appendChild(e);
+e = document.createElement('script');
+e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+e.async = true;
+document.getElementById('fb-root').appendChild(e);
--- static/js/facebook.js
+++ static/js/facebook.js
@@ -1,20 +1,22 @@
 (function() {
+  var e;
 
-  $(document).ready(function() {
-    var e;
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId: '281749461905114',
-        status: true,
-        cookie: true,
-        xfbml: true
-      });
-      return FB.Canvas.setAutoGrow();
-    };
-    e = document.createElement('script');
-    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-    e.async = true;
-    return document.getElementById('fb-root').appendChild(e);
-  });
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: '281749461905114',
+      status: true,
+      cookie: true,
+      xfbml: true
+    });
+    return FB.Canvas.setAutoGrow();
+  };
+
+  e = document.createElement('script');
+
+  e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+
+  e.async = true;
+
+  document.getElementById('fb-root').appendChild(e);
 
 }).call(this);
