59d66475a9aa5addfe0cc3c5b2cb1fedb88d8ee6
Fayland Lam <fayland@gmail.com>
2012-04-15 22:37:07 +0800

    conf.sh

---
+++ etc/conf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s /findmjob.com/etc/nginx.findmjob.com.conf /etc/nginx/sites-enabled/nginx.findmjob.com.conf;
+ln -s /findmjob.com/etc/supervise/www/ /service/www;
+ln -s /findmjob.com/etc/supervise/api/ /service/api;
+
