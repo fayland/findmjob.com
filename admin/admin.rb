6a64aa10487101bba659002995739d1e3f0fdb14
Fayland Lam <fayland@gmail.com>
2012-04-17 21:03:15 +0800

    merge config

--- admin/admin.rb
+++ admin/admin.rb
@@ -7,10 +7,16 @@ set :public_folder, File.dirname(__FILE__) + '/../static'
 set :views, File.dirname(__FILE__) + '/templates'
 
 configure do
-  set :config, YAML::load( File.read(File.dirname(__FILE__) + '/../conf/findmjob.yml') )
+  config = YAML::load( File.read(File.dirname(__FILE__) + '/../conf/findmjob.yml') )
+  config_local = YAML::load( File.read(File.dirname(__FILE__) + '/../conf/findmjob_local.yml') )
+  set :config, config.merge(config_local)
+end
+
+before do
+  @config = settings.config
 end
 
 get '/' do
-  @config = settings.config
+
   erb :index
 end
