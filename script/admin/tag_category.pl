03ba8d594316d1ea3633615653f76c74edbd679a
Fayland Lam <fayland@gmail.com>
2012-04-19 09:54:19 +0800

    more keywords

--- script/admin/tag_category.pl
+++ script/admin/tag_category.pl
@@ -8,10 +8,10 @@ use FindmJob::Basic;
 
 my $dbh = FindmJob::Basic->dbh;
 
-my @language = ('perl', 'python', 'java', 'asp', 'php', 'javascript', 'ruby', 'c#', 'c++', 'lisp', 'actionscript', 'c', 'objective-c', 'visual basic', 'sql', '.net', 'asp.net', 'pascal', 'lua', 'fortran', 'scheme', 'bash', 'haskell', 'smalltalk', 'erlang', 'groovy', 'prolog', 'html', 'css', 'scala', 'vbscript', 'clojure', 'ColdFusion', 'OCaml', 'Smalltalk');
+my @language = ('perl', 'python', 'java', 'asp', 'php', 'javascript', 'ruby', 'c#', 'c++', 'lisp', 'actionscript', 'c', 'objective-c', 'visual basic', 'sql', '.net', 'asp.net', 'pascal', 'lua', 'fortran', 'scheme', 'bash', 'haskell', 'smalltalk', 'erlang', 'groovy', 'prolog', 'html', 'css', 'scala', 'vbscript', 'clojure', 'ColdFusion', 'OCaml', 'Smalltalk', 'shell');
 $dbh->do("UPDATE tag SET category='language' WHERE text IN (" . join(', ', split(//, '?' x @language)) . ")", undef, @language);
 
-my @skills = ('MySQL', 'Linux', 'svn', 'git', 'Apache', 'Ajax', 'node.js', 'coffeescript', 'WordPress', 'Joomla', 'Catalyst', 'DBIx::Class', 'Moose', 'Drupal', 'Ruby on Rails', 'Dancer', 'Mojo', 'ruby-on-rails', 'photoshop', 'facebook', 'hadoop', 'silverlight', 'oracle', 'postgresql', 'iPad', 'iPhone', 'jquery', 'html5', 'unix', 'redis', 'twitter', 'mongodb', 'android', 'css3', 'jquery-ui', 'memcached', 'nosql', 'django', 'oracle');
+my @skills = ('MySQL', 'Linux', 'svn', 'git', 'Apache', 'Ajax', 'node.js', 'coffeescript', 'WordPress', 'Joomla', 'Catalyst', 'DBIx::Class', 'Moose', 'Drupal', 'Ruby on Rails', 'Dancer', 'Mojo', 'ruby-on-rails', 'photoshop', 'facebook', 'hadoop', 'silverlight', 'oracle', 'postgresql', 'iPad', 'iPhone', 'jquery', 'html5', 'unix', 'redis', 'twitter', 'mongodb', 'android', 'css3', 'jquery-ui', 'memcached', 'nosql', 'django', 'oracle', 'flex', 'flash', 'phonegap', 'appmobi');
 $dbh->do("UPDATE tag SET category='skill' WHERE text IN (" . join(', ', split(//, '?' x @skills)) . ")", undef, @skills);
 
 1;
