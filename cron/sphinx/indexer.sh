ecb6d77c5d3b159b9e5900f21750c7700fbdcf10
Fayland Lam <fayland@gmail.com>
2012-04-19 11:09:07 +0800

    fix path

--- cron/sphinx/indexer.sh
+++ cron/sphinx/indexer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-svstat /service/sphinx | awk -F '\)| ' '{print $4}' > /findmjob.com/log/searchd.pid;
+/usr/local/bin/svstat /service/sphinx | awk -F '\)| ' '{print $4}' > /findmjob.com/log/searchd.pid;
 /usr/local/bin/indexer --all --config /findmjob.com/etc/sphinx.conf --rotate;
