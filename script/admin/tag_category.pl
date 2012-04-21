807042c4fe1c7223627fcc066c6ec48775f9079b
Fayland Lam <fayland@gmail.com>
2012-04-21 13:24:24 +0800

    more tags

--- script/admin/tag_category.pl
+++ script/admin/tag_category.pl
@@ -11,7 +11,7 @@ my $dbh = FindmJob::Basic->dbh;
 my @language = ('perl', 'python', 'java', 'asp', 'php', 'javascript', 'ruby', 'c#', 'c++', 'lisp', 'actionscript', 'c', 'objective-c', 'visual basic', 'sql', '.net', 'asp.net', 'pascal', 'lua', 'fortran', 'scheme', 'bash', 'haskell', 'smalltalk', 'erlang', 'groovy', 'prolog', 'html', 'xml', 'css', 'scala', 'vbscript', 'clojure', 'ColdFusion', 'OCaml', 'Smalltalk', 'shell');
 $dbh->do("UPDATE tag SET category='language' WHERE text IN (" . join(', ', split(//, '?' x @language)) . ")", undef, @language);
 
-my @skills = ('MySQL', 'Linux', 'svn', 'git', 'Apache', 'Ajax', 'node.js', 'coffeescript', 'WordPress', 'Joomla', 'Catalyst', 'DBIx::Class', 'Moose', 'Drupal', 'Ruby on Rails', 'Dancer', 'Mojo', 'ruby-on-rails', 'photoshop', 'facebook', 'hadoop', 'silverlight', 'oracle', 'postgresql', 'iPad', 'iPhone', 'jquery', 'html5', 'unix', 'redis', 'twitter', 'mongodb', 'android', 'css3', 'jquery-ui', 'memcached', 'nosql', 'django', 'oracle', 'flex', 'flash', 'phonegap', 'appmobi', 'telecommute', 'telecommuting', 'cPanel');
+my @skills = ('MySQL', 'Linux', 'svn', 'git', 'Apache', 'Ajax', 'node.js', 'coffeescript', 'WordPress', 'Joomla', 'Catalyst', 'DBIx::Class', 'Moose', 'Drupal', 'Ruby on Rails', 'Dancer', 'Mojo', 'ruby-on-rails', 'photoshop', 'facebook', 'hadoop', 'silverlight', 'oracle', 'postgresql', 'iPad', 'iPhone', 'jquery', 'html5', 'unix', 'redis', 'twitter', 'mongodb', 'android', 'css3', 'jquery-ui', 'memcached', 'nosql', 'django', 'oracle', 'flex', 'flash', 'phonegap', 'appmobi', 'telecommute', 'telecommuting', 'cPanel', 'MongoDB', 'Redis', 'Cassandra', 'CouchDB', 'Riak');
 $dbh->do("UPDATE tag SET category='skill' WHERE text IN (" . join(', ', split(//, '?' x @skills)) . ")", undef, @skills);
 
 1;
