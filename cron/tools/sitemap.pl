9053a25fa64e89a4c3372d9a4f9df79993c442bd
Fayland Lam <fayland@gmail.com>
2012-05-03 21:18:16 +0800

    change sitemap with index

--- cron/tools/sitemap.pl
+++ cron/tools/sitemap.pl
@@ -5,20 +5,49 @@ use warnings;
 use FindBin qw/$Bin/;
 use lib "$Bin/../../lib";
 use FindmJob::Basic;
+use WWW::SitemapIndex::XML;
 use WWW::Sitemap::XML;
 use LWP::UserAgent;
 use URI::Escape;
 
 my $root = FindmJob::Basic->root;
-my $file = "$root/static/sitemap.xml.gz";
+my $dir = "$root/static/";
 
-my $map = WWW::Sitemap::XML->new();
 my $config = FindmJob::Basic->config;
 my $schema = FindmJob::Basic->schema;
 my $dbh = FindmJob::Basic->dbh;
 
 my @d = localtime();
 my $today = sprintf('%04d-%02d-%02d', $d[5] + 1900, $d[4] + 1, $d[3]);
+
+## an index with 4 files, jobs, freelancer, tags and companies
+my $index = WWW::SitemapIndex::XML->new();
+$index->add(
+    loc => $config->{sites}->{main} . '/sitemap.jobs.xml.gz',
+    lastmod => $today,
+);
+$index->add(
+    loc => $config->{sites}->{main} . '/sitemap.freelances.xml.gz',
+    lastmod => $today,
+);
+$index->add(
+    loc => $config->{sites}->{main} . '/sitemap.tags.xml.gz',
+    lastmod => $today,
+);
+
+=pod
+
+$index->add(
+    loc => $config->{sites}->{main} . '/sitemap.companies.xml.gz',
+    lastmod => $today,
+);
+
+=cut
+
+$index->write("$dir/sitemap.xml.gz");
+
+# sitemaps.jobs.xml.gz
+my $map = WWW::Sitemap::XML->new();
 $map->add(
     loc => 'http://findmjob.com/',
     lastmod => $today,
@@ -27,27 +56,54 @@ $map->add(
 );
 
 # Each text file can contain a maximum of 50,000 URLs and must be no larger than 10MB (10,485,760 bytes)
-my $sth = $dbh->prepare("SELECT id, tbl FROM object ORDER BY time DESC LIMIT 10000");
-$sth->execute();
-while (my ($id, $tbl) = $sth->fetchrow_array) {
-    my $obj = $schema->resultset(ucfirst $tbl)->find($id);
-    next unless $obj; # it should be warned
-    my $url = $config->{sites}->{main} . '/' . $tbl . '/' . $id;
-    $url = $config->{sites}->{main} . $obj->url if $obj->can('url');
-    if ($tbl eq 'job' or $tbl eq 'freelance') {
-        $map->add(
-            loc => $url,
-            priority => 0.7,
-        );
-    } else {
-        $map->add(
-            loc => $url,
-            priority => 0.5,
-        );
-    }
+my $rs = $schema->resultset('Job')->search( {
+    expired_at => { '>', \"NOW()" }, #"
+}, {
+    order_by => 'inserted_at DESC',
+    rows => 10000,
+    page => 1
+} );
+while (my $r = $rs->next) {
+    my $url = $config->{sites}->{main} . $r->url;
+    $map->add(
+        loc => $url,
+        priority => 0.7,
+    );
 }
+$map->write("$dir/sitemap.jobs.xml.gz");
 
-$map->write($file);
+# sitemap.freelances.xml.gz
+$map = WWW::Sitemap::XML->new();
+$rs = $schema->resultset('Freelance')->search( {
+    expired_at => { '>', \"NOW()" }, #"
+}, {
+    order_by => 'inserted_at DESC',
+    rows => 10000,
+    page => 1
+} );
+while (my $r = $rs->next) {
+    my $url = $config->{sites}->{main} . $r->url;
+    $map->add(
+        loc => $url,
+        priority => 0.7,
+    );
+}
+$map->write("$dir/sitemap.freelances.xml.gz");
+
+# sitemap.tags.xml.gz
+$map = WWW::Sitemap::XML->new();
+$rs = $schema->resultset('Tag')->search( undef, {
+    rows => 10000,
+    page => 1
+} );
+while (my $r = $rs->next) {
+    my $url = $config->{sites}->{main} . '/tag' . $r->id;
+    $map->add(
+        loc => $url,
+        priority => 0.5,
+    );
+}
+$map->write("$dir/sitemap.tags.xml.gz");
 
 # add ping
 LWP::UserAgent->new->get( "http://www.google.com/webmasters/tools/ping?sitemap=" . uri_escape($config->{sites}->{main} . '/sitemap.xml.gz') );
