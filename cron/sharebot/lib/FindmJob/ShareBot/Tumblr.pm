bc65b57abb9f6ad052131841708a7476d0e7e159
Fayland Lam <fayland@gmail.com>
2012-06-08 17:06:12 +0800

    source_url

--- cron/sharebot/lib/FindmJob/ShareBot/Tumblr.pm
+++ cron/sharebot/lib/FindmJob/ShareBot/Tumblr.pm
@@ -34,7 +34,7 @@ sub share {
        tags => $tags,
        url  => $url,
        name => $job->title,
-       source => $url,
+       source_url => $url,
     );
     my $st = 0;
     if ( $dispatch->is_success ) {
