c4471cf2ac0a48216fd3a5174449ec3780d11799
Fayland Lam <fayland@gmail.com>
2012-08-07 20:48:47 +0800

    with log_to_stdout=1

--- cron/lib/FindmJob/Role/Logger.pm
+++ cron/lib/FindmJob/Role/Logger.pm
@@ -6,5 +6,7 @@ use namespace::autoclean;
 with 'MooseX::Role::Loggable';
 
 has 'debug' => (is => 'rw', isa => 'Bool', default => 1);
+has 'log_to_stdout' => (is => 'ro', isa => 'Bool', default => 1);
+has 'log_to_stderr' => (is => 'ro', isa => 'Bool', default => 1);
 
 1;
