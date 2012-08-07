2de153ea96a0e761239dd1e87607d887f3da6de0
Fayland Lam <fayland@gmail.com>
2012-08-07 22:13:53 +0800

    temp facebook script

---
+++ script/oneoff/facebook_oauth_token.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use lib "$Bin/../../lib";
+use FindmJob::Basic;
+use autodie;
+use Data::Dumper;
+use Facebook::Graph;
+
+my $config = FindmJob::Basic->config;
+my $t = $config->{share}->{Facebook};
+my $fb = Facebook::Graph->new(
+    app_id   => $t->{app_id},
+    secret   => $t->{secret},
+    postback => 'http://fb.findmjob.com/'
+);
+
+my $file = "$Bin/facebook.token.txt";
+if (@ARGV) {
+    $fb->request_access_token(shift @ARGV);
+    open(my $fh, '>', "$Bin/facebook.token.txt");
+    print $fh $fb->access_token;
+    close($fh);
+} else {
+    my $uri = $fb
+        ->authorize
+        ->extend_permissions(qw(offline_access publish_stream))
+        ->uri_as_string;
+    print $uri . "\n";
+}
+
+1;
