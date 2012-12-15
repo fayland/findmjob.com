b5d88b70fed0d39cb107176dd857497424c6d4ce
Fayland Lam <fayland@gmail.com>
2012-12-15 10:10:24 +0800

    changes on the logger

--- cron/lib/FindmJob/Role/Logger.pm
+++ cron/lib/FindmJob/Role/Logger.pm
@@ -6,7 +6,11 @@ use namespace::autoclean;
 with 'MooseX::Role::Loggable';
 
 has 'debug' => (is => 'rw', isa => 'Bool', default => 1);
-has 'log_to_stdout' => (is => 'ro', isa => 'Bool', default => 0);
+has 'log_to_stdout' => (is => 'ro', isa => 'Bool', default => 1);
 has 'log_to_stderr' => (is => 'ro', isa => 'Bool', default => 0);
+has 'logger_facility' => (is => 'ro', isa => 'Str', default => 'none');
+has 'logger_ident' => (is => 'ro', isa => 'Str', default => sub { 'findmjob' });
+has 'log_to_file' => (is => 'ro', isa => 'Bool', default => 1);
+has 'log_path' => (is => 'ro', isa => 'Str', predicate => 'has_log_path', default => '/tmp');
 
 1;
