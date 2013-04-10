5475c9a023a4fcaba114c2c2a186f9c015c5a59d
Fayland Lam <fayland@gmail.com>
2013-04-10 18:59:08 +0800

    small fix

--- bin/www.pl
+++ bin/www.pl
@@ -3,10 +3,11 @@
 use strict;
 use warnings;
 
-use lib 'lib';
-use Mojolicious::Commands;
+use FindBin;
+BEGIN { unshift @INC, "$FindBin::Bin/../lib" }
 
 # Start command line interface for application
+require Mojolicious::Commands;
 Mojolicious::Commands->start_app('FindmJob::WWW');
 
 1;
