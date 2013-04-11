f9ffd7ef131e427ca66a445a973fab432ee8de30
Fayland Lam <fayland@gmail.com>
2013-04-11 23:22:30 +0800

    do it in one week so things changes

--- cron/stats/location_job_num.pl
+++ cron/stats/location_job_num.pl
@@ -10,7 +10,9 @@ use Data::Dumper;
 my $dbh = FindmJob::Basic->dbh;
 
 my $update_sth = $dbh->prepare("UPDATE location SET job_num = ? WHERE id = ?");
-my $sth = $dbh->prepare("SELECT location_id, COUNT(*) FROM job GROUP BY location");
+
+my $one_week = time() - 7 * 86400;
+my $sth = $dbh->prepare("SELECT location_id, COUNT(*) FROM job WHERE inserted_at > $one_week GROUP BY location");
 $sth->execute();
 while (my ($id, $cnt) = $sth->fetchrow_array) {
     $update_sth->execute($cnt, $id);
