97af1f80802f93d687df6e0a03d732700ca0dadb
Fayland Lam <fayland@gmail.com>
2013-04-12 14:48:06 +0800

    for job too

--- cron/cleanup/expired.pl
+++ cron/cleanup/expired.pl
@@ -7,10 +7,9 @@ use lib "$Bin/../../lib"; # FindmJob::Basic
 use FindmJob::Basic;
 use Data::Dumper;
 
-## remove old freelances which is older than one months
-
 my $dbh = FindmJob::Basic->dbh;
 
+## remove old freelances which is older than one months
 my $month_ago = time() - 40 * 86400;
 my $rows = $dbh->do("DELETE FROM freelance WHERE inserted_at < $month_ago");
 print "DELETE freelance: $rows\n";
@@ -18,6 +17,15 @@ print "DELETE freelance: $rows\n";
 $rows = $dbh->do("DELETE FROM object where tbl='freelance' and id not in (SELECT id FROM freelance)");
 print "DELETE object: $rows\n";
 
+## jobs, for 3 months
+$month_ago = time() - 100 * 86400;
+$rows = $dbh->do("DELETE FROM job WHERE inserted_at < $month_ago");
+print "DELETE freelance: $rows\n";
+
+$rows = $dbh->do("DELETE FROM object where tbl='job' and id not in (SELECT id FROM job)");
+print "DELETE object: $rows\n";
+
+# for both job and freelance
 $rows = $dbh->do("DELETE FROM object_tag where object not in (SELECT id FROM object)");
 print "DELETE object_tag: $rows\n";
 
