9492ad17f3b068ffa149ac32825274f7286102d8
Fayland Lam <fayland@gmail.com>
2013-04-12 14:51:36 +0800

    word fix

--- cron/cleanup/expired.pl
+++ cron/cleanup/expired.pl
@@ -15,15 +15,15 @@ my $rows = $dbh->do("DELETE FROM freelance WHERE inserted_at < $month_ago");
 print "DELETE freelance: $rows\n";
 
 $rows = $dbh->do("DELETE FROM object where tbl='freelance' and id not in (SELECT id FROM freelance)");
-print "DELETE object: $rows\n";
+print "DELETE freelance object: $rows\n";
 
 ## jobs, for 3 months
 $month_ago = time() - 100 * 86400;
 $rows = $dbh->do("DELETE FROM job WHERE inserted_at < $month_ago");
-print "DELETE freelance: $rows\n";
+print "DELETE job: $rows\n";
 
 $rows = $dbh->do("DELETE FROM object where tbl='job' and id not in (SELECT id FROM job)");
-print "DELETE object: $rows\n";
+print "DELETE job object: $rows\n";
 
 # for both job and freelance
 $rows = $dbh->do("DELETE FROM object_tag where object not in (SELECT id FROM object)");
