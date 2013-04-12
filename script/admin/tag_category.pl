5c8bae0f524a0e83bd9e9e87679b52acd2be88eb
Fayland Lam <fayland@gmail.com>
2013-04-12 21:59:04 +0800

    add more tags

--- script/admin/tag_category.pl
+++ script/admin/tag_category.pl
@@ -13,7 +13,7 @@ my $select_sth = $dbh->prepare("SELECT id FROM tag WHERE text = ?");
 my $insert_sth = $dbh->prepare("INSERT INTO tag (id, text, category) VALUES (?, ?, ?)");
 my $update_sth = $dbh->prepare("UPDATE tag SET category = ? WHERE id = ?");
 
-my @language = ('perl', 'python', 'java', 'asp', 'php', 'javascript', 'ruby', 'c#', 'c++', 'lisp', 'actionscript', 'c', 'objective-c', 'visual basic', 'sql', '.net', 'asp.net', 'pascal', 'lua', 'fortran', 'scheme', 'bash', 'haskell', 'smalltalk', 'erlang', 'groovy', 'prolog', 'html', 'xml', 'css', 'scala', 'vbscript', 'clojure', 'ColdFusion', 'OCaml', 'Smalltalk', 'shell');
+my @language = ('perl', 'python', 'java', 'asp', 'php', 'javascript', 'ruby', 'c#', 'c++', 'lisp', 'actionscript', 'c', 'objective-c', 'visual basic', 'sql', '.net', 'asp.net', 'pascal', 'lua', 'fortran', 'scheme', 'bash', 'haskell', 'smalltalk', 'erlang', 'groovy', 'prolog', 'html', 'xml', 'css', 'scala', 'vbscript', 'clojure', 'ColdFusion', 'OCaml', 'Smalltalk', 'shell', 'Dart');
 foreach my $tag (@language) {
     $select_sth->execute($tag);
     my ($id) = $select_sth->fetchrow_array;
@@ -25,7 +25,7 @@ foreach my $tag (@language) {
     }
 }
 
-my @skills = ('MySQL', 'Linux', 'svn', 'git', 'Apache', 'Ajax', 'node.js', 'coffeescript', 'WordPress', 'Joomla', 'Catalyst', 'DBIx::Class', 'Moose', 'Drupal', 'Ruby on Rails', 'Dancer', 'Mojo', 'ruby-on-rails', 'photoshop', 'facebook', 'hadoop', 'silverlight', 'oracle', 'postgresql', 'iPad', 'iPhone', 'jquery', 'html5', 'unix', 'redis', 'mongodb', 'android', 'css3', 'jquery-ui', 'memcached', 'nosql', 'django', 'oracle', 'flex', 'flash', 'phonegap', 'appmobi', 'telecommute', 'telecommuting', 'cPanel', 'MongoDB', 'Redis', 'Cassandra', 'CouchDB', 'Riak', 'Amazon Web Services', 'AWS', 'SOAP', 'REST', 'OAuth', 'OpenID', 'SEO', 'Rails', 'iOS');
+my @skills = ('MySQL', 'Linux', 'svn', 'git', 'Apache', 'Ajax', 'node.js', 'coffeescript', 'WordPress', 'Joomla', 'Catalyst', 'DBIx::Class', 'Moose', 'Drupal', 'Ruby on Rails', 'Dancer', 'Mojo', 'ruby-on-rails', 'photoshop', 'facebook', 'hadoop', 'silverlight', 'oracle', 'postgresql', 'iPad', 'iPhone', 'jquery', 'html5', 'unix', 'redis', 'mongodb', 'android', 'css3', 'jquery-ui', 'jQuery Mobile', 'memcached', 'nosql', 'django', 'oracle', 'flex', 'flash', 'Cordova', 'phonegap', 'appmobi', 'telecommute', 'telecommuting', 'cPanel', 'MongoDB', 'Redis', 'Cassandra', 'CouchDB', 'Riak', 'Amazon Web Services', 'AWS', 'SOAP', 'REST', 'OAuth', 'OpenID', 'SEO', 'Rails', 'iOS');
 foreach my $tag (@skills) {
     $select_sth->execute($tag);
     my ($id) = $select_sth->fetchrow_array;
