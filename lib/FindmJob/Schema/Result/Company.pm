ba7dc0ae2e00ce35029d82f2dd8d0ebf555ea23c
Fayland Lam <fayland@gmail.com>
2013-11-03 19:24:53 +0800

    decode_utf8

--- lib/FindmJob/Schema/Result/Company.pm
+++ lib/FindmJob/Schema/Result/Company.pm
@@ -95,9 +95,10 @@ sub title { (shift)->name }
 
 # extra data
 use JSON::XS;
+use Encode;
 sub extra_data {
     my $extra = (shift)->extra;
-    return ($extra and $extra =~ /^\{/) ? JSON::XS->new->utf8->decode($extra) : {};
+    return ($extra and $extra =~ /^\{/) ? JSON::XS->new->utf8->decode( decode_utf8($extra) ) : {};
 }
 
 1;
