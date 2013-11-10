e7a89b82d97b5dedf2c78364222808e0a4088d2c
Fayland Lam <fayland@gmail.com>
2013-11-10 13:32:40 +0800

    bigger no limit as twitter

--- cron/sharebot/lib/FindmJob/ShareBot/GooglePlus.pm
+++ cron/sharebot/lib/FindmJob/ShareBot/GooglePlus.pm
@@ -2,7 +2,6 @@ package FindmJob::ShareBot::GooglePlus;
 
 use Moo;
 with 'FindmJob::ShareBot::Role';
-with 'FindmJob::Role::Shorten';
 
 use List::Util 'shuffle';
 
@@ -22,11 +21,7 @@ sub share {
 
     my $config = $self->config;
     my $url = $config->{sites}->{main} . $job->url;
-    my $shorten_url = $self->shorten($url);
-
-    my $left_len = 140 - (length($tags) + length($shorten_url) + 2);
     my $title = $job->title;
-    $title = substr($title, 0, $left_len - 3) . '...' if length($title) > $left_len;
 
     my $t = $config->{share}->{GooglePlus};
     my $root = $self->root;
@@ -36,10 +31,10 @@ sub share {
     my $casperjs  = "$root/bin/casperjs/bin/casperjs";
     my $js_file   = "$root/bin/googleplus.js";
 
-    my $update = "$title $shorten_url $tags";
+    my $update = "$title $url $tags";
     $self->log_debug("# ++ $update");
 
-    my $command = qq~$casperjs $js_file --username=$username --password=$password --text="$update"~;
+    my $command = qq~$casperjs $js_file --cookies-file=$root/bin/googleplus_cookies.txt --username=$username --password=$password --text="$update"~;
     my $out = `$command 2>&1`;
     $self->log_debug( "# out: $out" );
 
