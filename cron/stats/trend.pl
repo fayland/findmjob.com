62dca61dc3673c071f6380af1f9b22c45d8903b1
Fayland Lam <fayland@gmail.com>
2013-11-23 22:19:03 +0800

    add trend

---
+++ cron/stats/trend.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use lib "$Bin/../../lib"; # FindmJob::Basic
+use FindmJob::Basic;
+use Data::Dumper;
+
+my $dbh = FindmJob::Basic->dbh;
+my $rows = $dbh->do(<<SQL);
+INSERT IGNORE INTO stats_trend SELECT DATE(FROM_UNIXTIME(time)) as dt, tag, COUNT(*) FROM object_tag WHERE tag in (SELECT id FROM tag WHERE category='language' or category='skill') AND DATE(FROM_UNIXTIME(time)) >= date(date_sub(now(), interval 2 day)) AND DATE(FROM_UNIXTIME(time)) < date(now()) GROUP BY dt, tag;
+SQL
+
+print "Done\n";
+
+1;
