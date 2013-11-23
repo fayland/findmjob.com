16a5cce4f3dda65452b36cbd634916954ea476a5
Fayland Lam <fayland@gmail.com>
2013-11-23 20:14:00 +0800

    grunt uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,35 +5,48 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     cssmin: {
-	  combine: {
-	    files: {
-	      'static/assets/css/findmjob.min.css': [
-	      	'static/bootstrap/css/bootstrap.min.css',
-	      	'static/assets/css/font-awesome.min.css',
-	      	'static/assets/css/findmjob.css'
-	      ]
-	    }
-	  }
-	},
-	coffee: {
-	  compile: {
-	  	files: {
-	  	  'static/js/facebook.js': 'static/js/facebook.coffee'
-	  	}
-	  },
-	  compileJoined: {
-	    options: {
-	      join: true
-	    },
-	    files: {
-	      'static/js/findmjob.js': 'static/js/coffee/*.coffee'
-	    }
-	  }
-	}
+      combine: {
+        files: {
+          'static/assets/css/findmjob.min.css': [
+            'static/bootstrap/css/bootstrap.min.css',
+            'static/assets/css/font-awesome.min.css',
+            'static/assets/css/findmjob.css'
+          ]
+        }
+      }
+    },
+    coffee: {
+      compile: {
+        files: {
+          'static/js/facebook.js': 'static/js/facebook.coffee'
+        }
+      },
+      compileJoined: {
+        options: {
+          join: true
+        },
+        files: {
+          'static/js/findmjob.js': 'static/js/coffee/*.coffee'
+        }
+      }
+    },
+    uglify: {
+      build: {
+        src: [
+          'static/js/jquery.min.js',
+          'static/bootstrap/js/bootstrap.min.js',
+          'static/js/jquery.timeago.js',
+          'static/js/findmjob.js',
+          'static/js/ga.js'
+        ],
+        dest: 'static/js/findmjob.min.js'
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['cssmin', 'coffee']);
+  grunt.registerTask('default', ['cssmin', 'coffee', 'uglify']);
 };
