d0634cbbc5a9f8afca508886274d4ddd4e096b45
Fayland Lam <fayland@gmail.com>
2013-11-23 22:49:43 +0800

    for trends

--- lib/FindmJob/WWW/Trends.pm
+++ lib/FindmJob/WWW/Trends.pm
@@ -8,7 +8,7 @@ sub index {
     my $schema = $self->schema;
     my $dbh = $schema->storage->dbh;
 
-    my (%languages, %skills);
+    my (%languages, %skills, %total_count);
     my $sth = $dbh->prepare(<<SQL);
 SELECT tr.*, tag.text, tag.category FROM stats_trends tr JOIN tag ON tr.tag_id=tag.id WHERE tr.dt > DATE_SUB(NOW(), INTERVAL 30 DAY) order by dt;
 SQL
@@ -19,11 +19,19 @@ SQL
         } else {
             push @{ $skills{$row->{text}} }, $row;
         }
+        $total_count{$row->{category}}{$row->{text}} += $row->{num};
     }
+
+    $total_count{language} ||= {};
+    $total_count{skill} ||= {};
+    my @languages = sort { $total_count{language}{$b} <=> $total_count{language}{$a} } keys %{ $total_count{language} };
+    my @skills = sort { $total_count{skill}{$b} <=> $total_count{skill}{$a} } keys %{ $total_count{skill} };
 
     $self->stash(
         languages => \%languages,
         skills => \%skills,
+        top10_languages => [splice(@languages, 0, 15)],
+        top10_skills => [splice(@skills, 0, 15)],
     );
 }
 
